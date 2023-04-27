a34d5860d5b427aa1e766232679280909347cb8c
Fabrice K <kerremans.1904.student@ifosupwavre.be>
2023-04-27 18:26:07 +0200

    Ajoutez des fichiers projet.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace fabriceCar;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace fabriceCar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
